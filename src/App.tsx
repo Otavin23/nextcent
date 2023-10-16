880e175ad7c516d8bb25fcd6356da937e1036d28
Otavin23 <alvesotavio132@gmail.com>
2023-10-16 16:58:29 -0300

    :lipstick: style: section of our customers with a list of companies

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChakraBaseProvider } from "@chakra-ui/react";
+import { ChakraBaseProvider, UnorderedList } from "@chakra-ui/react";
 import { themeChakra } from "./style/theme";
 import { Header } from "./Components/Header";
 import {
@@ -46,6 +46,36 @@ const App = () => {
           </Box>
 
           <Image src="../assets/Illustration.svg" alt="" w="500px" h="500px" />
+        </Container>
+      </Flex>
+
+      <Flex justify="center" py="3rem">
+        <Container
+          variant="mainLine"
+          display="Flex"
+          alignItems="center"
+          flexDirection="column"
+        >
+          <Heading color="#4D4D4D" fontWeight="600">
+            Our Clients
+          </Heading>
+          <Text my="1rem" color="#717171">
+            We have been working with some Fortune 500+ clients
+          </Text>
+          <Flex justify="space-between" align="center" w="100%">
+            <Image
+              src="../assets/clients/Vector.svg"
+              alt=""
+              w="65px"
+              h="65px"
+            />
+            <Image src="../assets/clients/Logo.svg" alt="" w="65px" h="65px" />
+            <Image src="../assets/clients/Logo1.svg" alt="" w="65px" h="65px" />
+            <Image src="../assets/clients/Logo2.svg" alt="" w="65px" h="65px" />
+            <Image src="../assets/clients/Logo3.svg" alt="" w="65px" h="65px" />
+            <Image src="../assets/clients/Logo4.svg" alt="" w="65px" h="65px" />
+            <Image src="../assets/clients/Logo1.svg" alt="" w="65px" h="70px" />
+          </Flex>
         </Container>
       </Flex>
     </ChakraBaseProvider>
