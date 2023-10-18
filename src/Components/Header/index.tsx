d06247342481b2532b4aec5ae7c8ace253ce027e
Otavin23 <alvesotavio132@gmail.com>
2023-10-18 14:57:09 -0300

    :lipstick: style: none responsive header in nav and flex in menu

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -5,11 +5,21 @@ import {
   UnorderedList,
   Button,
   ListItem,
+  Box,
 } from "@chakra-ui/react";
 
 const Header = () => {
   return (
-    <Flex as="header" justify="center" py="1rem">
+    <Flex
+      as="header"
+      justify="center"
+      py="1rem"
+      pos="fixed"
+      w="100%"
+      bg="#fff"
+      zIndex="1"
+      borderBottom="1px solid #c1c1c147"
+    >
       <Container
         variant="mainLine"
         display="flex"
@@ -17,19 +27,31 @@ const Header = () => {
         alignItems="center"
       >
         <Image src="../assets/Logo.svg" alt="" w="150px" />
-        <Flex as="nav">
+
+        <Flex as="nav" align="center" display={{ base: "none", md: "flex" }}>
           <UnorderedList
             m="0"
             display="flex"
             alignItems="center"
             listStyleType="none"
           >
-            <ListItem mr="3rem">Home</ListItem>
-            <ListItem mr="3rem">Features</ListItem>
-            <ListItem mr="3rem">Community</ListItem>
-            <ListItem mr="3rem">Blog</ListItem>
-            <ListItem mr="3rem">Pricing</ListItem>
+            <ListItem mr={{ lg: "3rem", md: "1rem", sm: "0.8rem" }}>
+              Home
+            </ListItem>
+            <ListItem mr={{ lg: "3rem", md: "1rem", sm: "0.8rem" }}>
+              Features
+            </ListItem>
+            <ListItem mr={{ lg: "3rem", md: "1rem", sm: "0.8rem" }}>
+              Community
+            </ListItem>
+            <ListItem mr={{ lg: "3rem", md: "1rem", sm: "0.8rem" }}>
+              Blog
+            </ListItem>
+            <ListItem mr={{ lg: "3rem", md: "1rem", sm: "0.8rem" }}>
+              Pricing
+            </ListItem>
           </UnorderedList>
+
           <Button variant="noOutline" size="largePR">
             Register Now
             <Image
@@ -41,6 +63,13 @@ const Header = () => {
             />
           </Button>
         </Flex>
+
+        <Box
+          display={{ base: "flex", md: "none", sm: "flex" }}
+          cursor="pointer"
+        >
+          <Image src="../assets/menu.png" alt="" w="30px" h="30px" />
+        </Box>
       </Container>
     </Flex>
   );
